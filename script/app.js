3e1bf2a2b22bf1a2b2a6fba534bb98995f097ed8
MarcosWillianlA <marcoswillianl1987@gmail.com>
2024-10-08 13:21:53 -0400

    Quase finalizado
    A parte programática está praticamente concluída, irei fazer ajustes no CSS agora.

--- script/app.js
+++ script/app.js
@@ -6,3 +6,77 @@ const iniciarPausar = document.querySelector('#iniciar-pausar');
 const zerar = document.querySelector('#zerar');
 const cronometro = document.querySelector('#cronometro');
 
+let tempo;
+let marcandoTempo = false;
+let tempoRestante = 0;
+
+function configurarTempo(min) {
+    tempoRestante = min * 60;
+    atualizarCronometro();
+}
+
+function atualizarCronometro() {
+    const minutos = Math.floor(tempoRestante / 60);
+    const segundos = tempoRestante % 60; 
+    cronometro.textContent = `${String(minutos).padStart(2, '0')}:${String(segundos).padStart(2, '0')}`;
+}
+
+function iniciarEPausar() {
+    if (marcandoTempo) {
+        clearInterval(tempo);
+        marcandoTempo = false;
+        iniciarPausar.textContent = 'Iniciar';
+    } else {
+        tempo = setInterval(() => {
+            if (tempoRestante > 0) {
+                tempoRestante--;
+                atualizarCronometro();
+            } else {
+                clearInterval(tempo);
+                marcandoTempo = false;
+                iniciarPausar.textContent = 'Iniciar';
+            }
+        }, 1000); //Atualiza por segundo
+        marcandoTempo = true;
+        iniciarPausar.textContent = 'Pausar';
+    }
+}
+
+function zerarCronometro() {
+    iniciarPausar.innerHTML = 'Iniciar';
+    clearInterval(tempo);
+    tempoRestante = 0;
+    atualizarCronometro();
+    marcandoTempo(false);
+}
+
+atualizarCronometro();
+
+tempo25.addEventListener('click', () => {
+    configurarTempo(25);
+    atualizarCronometro();
+})
+
+tempo50.addEventListener('click', () => {
+    configurarTempo(50);
+    atualizarCronometro();
+})
+
+descanso05.addEventListener('click', () => {
+    configurarTempo(5);
+    atualizarCronometro();
+})
+
+descanso15.addEventListener('click', () => {
+    configurarTempo(15);
+    atualizarCronometro();
+})
+
+iniciarPausar.addEventListener('click', () => {
+    iniciarEPausar();
+})
+
+zerar.addEventListener('click', () => {
+    zerarCronometro();
+    atualizarCronometro();
+})
